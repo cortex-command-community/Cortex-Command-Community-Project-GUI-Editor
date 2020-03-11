4a06c0b596cd21ae62a2ea9b20aeace739a6d9bf
MaximDude <maximdude@gmail.com>
2020-03-12 00:06:21 +0200

    Project files for static GUI library

---
+++ GUILibrary/GUILibrary.cpp
@@ -0,0 +1,10 @@
+// GUI Library.cpp : Defines the functions for the static library.
+//
+
+#include "pch.h"
+#include "framework.h"
+
+// TODO: This is an example of a library function
+void fnGUILibrary()
+{
+}
