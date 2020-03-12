4de09acee2c814fdbfd9ed5d483435f4c7bdfc4b
MaximDude <maximdude@gmail.com>
2020-03-12 23:58:17 +0200

    Rename pch to LibraryIncludes

---
+++ GUILibrary/LibraryIncludes.cpp
@@ -0,0 +1,5 @@
+// LibraryIncludes.cpp: source file corresponding to the pre-compiled header
+
+#include "LibraryIncludes.h"
+
+// When you are using pre-compiled headers, this source file is necessary for compilation to succeed.
---
+++ GUILibrary/LibraryIncludes.h
@@ -0,0 +1,30 @@
+// LibraryIncludes.h: This is a precompiled header file.
+// Files listed below are compiled only once, improving build performance for future builds.
+// This also affects IntelliSense performance, including code completion and many code browsing features.
+// However, files listed here are ALL re-compiled if any one of them is updated between builds.
+// Do not add files here that you will be updating frequently as this negates the performance advantage.
+
+#ifndef _GUILIBINCLUDES_
+#define _GUILIBINCLUDES_      
+
+#include <cstdlib>
+#include <cstdarg>
+#include <cstddef>
+#include <cctype>
+#include <string>
+#include <cstring>
+#include <vector>
+#include <list>
+#include <set>
+#include <map>
+#include <unordered_map>
+#include <deque>
+#include <iostream>
+#include <fstream>
+#include <istream>
+#include <algorithm>
+#include <cmath>
+#include <cassert>
+#include <limits>
+
+#endif
