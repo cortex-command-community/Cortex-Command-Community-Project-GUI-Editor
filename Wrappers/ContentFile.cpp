88c1edf6bf7a02065519c05ee6de22f12c20c7fc
MaximDude <maximdude@gmail.com>
2020-11-14 02:41:56 +0200

    Comment out loading bitmaps from static map - iterator crashes for god knows what reason
    Cleanup ContentFile

--- Wrappers/ContentFile.cpp
+++ Wrappers/ContentFile.cpp
@@ -1,200 +1,66 @@
-//////////////////////////////////////////////////////////////////////////////////////////
-// File:            ContentFile.cpp
-//////////////////////////////////////////////////////////////////////////////////////////
-// Description:     Source file for the ContentFile class.
-// Project:         Retro Terrain Engine
-// Author(s):       Daniel Tabar
-//                  data@datarealms.com
-//                  http://www.datarealms.com
-
-
-//////////////////////////////////////////////////////////////////////////////////////////
-// Inclusions of header files
-
 #include "ContentFile.h"
 #include "RTEError.h"
 
+namespace RTE {
 
-using namespace std;
+	const string ContentFile::m_ClassName = "ContentFile";
+	std::unordered_map<std::string, BITMAP *> ContentFile::m_LoadedBitmaps;
 
-namespace RTE
-{
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-const string ContentFile::m_ClassName = "ContentFile";
-map<string, BITMAP *> ContentFile::m_sLoadedBitmaps;
+	int ContentFile::Create(const char *filePath) {
+		m_DataPath = filePath;
+		return 0;
+	}
 
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-//////////////////////////////////////////////////////////////////////////////////////////
-// Method:          Clear
-//////////////////////////////////////////////////////////////////////////////////////////
-// Description:     Clears all the member variables of this ContentFile, effectively
-//                  resetting the members of this abstraction level only.
+	int ContentFile::Create(const ContentFile &reference) {
+		m_DataPath = reference.m_DataPath;
+		return 0;
+	}
 
-void ContentFile::Clear()
-{
-    m_DataPath.erase();
-    m_DataModuleID = 0;
-}
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-//////////////////////////////////////////////////////////////////////////////////////////
-// Method:          Create
-//////////////////////////////////////////////////////////////////////////////////////////
-// Description:     Makes the ContentFile object ready for use.
+	void ContentFile::FreeAllLoaded() {
+		for (const std::pair<std::string, BITMAP *> &loadedBitmap : m_LoadedBitmaps) {
+			destroy_bitmap(loadedBitmap.second);
+		}
+	}
 
-int ContentFile::Create(const char *filePath)
-{
-    m_DataPath = filePath;
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-    return 0;
-}
+	BITMAP * ContentFile::GetAsBitmap(int conversionMode) {
+		if (m_DataPath.empty()) {
+			return nullptr;
+		}
+		BITMAP *returnBitmap = nullptr;
 
+		/* Iterator crashes for god knows what reason
+		std::unordered_map<std::string, BITMAP *>::iterator foundBitmap = m_LoadedBitmaps.find(m_DataPath);
+		if (foundBitmap != m_LoadedBitmaps.end()) {
+			returnBitmap = (*foundBitmap).second;
+		} else {
+			returnBitmap = LoadAndReleaseBitmap(conversionMode);
+			m_LoadedBitmaps.insert({ m_DataPath, returnBitmap });
+		}
+		*/
 
-//////////////////////////////////////////////////////////////////////////////////////////
-// Method:          Create
-//////////////////////////////////////////////////////////////////////////////////////////
-// Description:     Creates a ContentFile to be identical to another, by deep copy.
+		returnBitmap = LoadAndReleaseBitmap(conversionMode);
 
-int ContentFile::Create(const ContentFile &reference)
-{
-//    Entity::Create(reference);
+		return returnBitmap;
+	}
 
-    m_DataPath = reference.m_DataPath;
-    m_DataModuleID = reference.m_DataModuleID;
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-    return 0;
-}
+	BITMAP * ContentFile::LoadAndReleaseBitmap(int conversionMode) {
+		if (m_DataPath.empty()) {
+			return nullptr;
+		}
+		set_color_conversion(conversionMode == 0 ? COLORCONV_MOST : conversionMode);
 
-
-
-//////////////////////////////////////////////////////////////////////////////////////////
-// Method:          Destroy
-//////////////////////////////////////////////////////////////////////////////////////////
-// Description:     Destroys and resets (through Clear()) the ContentFile object.
-
-void ContentFile::Destroy(bool notInherited)
-{
-    Clear();
-}
-
-
-//////////////////////////////////////////////////////////////////////////////////////////
-// Static method:   FreeAllLoaded
-//////////////////////////////////////////////////////////////////////////////////////////
-// Description:     Frees all loaded data used by all ContentFile instances. This should
-//                  ONLY be done when quitting the app, or after everything else is
-//                  completly destroyed
-
-void ContentFile::FreeAllLoaded()
-{
-    for (map<string, BITMAP *>::iterator lbItr = m_sLoadedBitmaps.begin(); lbItr != m_sLoadedBitmaps.end(); ++lbItr)
-        destroy_bitmap((*lbItr).second);
-}
-
-
-//////////////////////////////////////////////////////////////////////////////////////////
-// Virtual method:  SetDataPath
-//////////////////////////////////////////////////////////////////////////////////////////
-// Description:     Sets the file path of the content file represented by this ContentFile
-//                  object.
-
-void ContentFile::SetDataPath(std::string newDataPath)
-{
-    m_DataPath = newDataPath;
-};
-
-
-//////////////////////////////////////////////////////////////////////////////////////////
-// Method:          GetDataModuleID
-//////////////////////////////////////////////////////////////////////////////////////////
-// Description:     Gets the ID of the Data Module this file is inside.
-// Arguments:       None.
-// Return value:    The ID of the Data Module containing this' file.
-
-int ContentFile::GetDataModuleID()
-{
-    return m_DataModuleID;
-}
-
-
-//////////////////////////////////////////////////////////////////////////////////////////
-// Method:          GetAsBitmap
-//////////////////////////////////////////////////////////////////////////////////////////
-// Description:     Loads and gets the data represtented by this ConentFile object as an
-//                  Allegro BITMAP. Note that ownership of the BITMAP IS NOT TRANSFERRED!
-
-BITMAP * ContentFile::GetAsBitmap(int conversionMode)
-{
-    if (m_DataPath.empty())
-        return 0;
-
-    BITMAP *pReturnBitmap = 0;
-
-    // Check if this file has already been read and loaded from disk.
-    map<string, BITMAP *>::iterator itr = m_sLoadedBitmaps.find(m_DataPath);
-    if (itr != m_sLoadedBitmaps.end())
-    {
-        // Yes, has been loaded previously, then use that data from memory.
-        pReturnBitmap = (*itr).second;
-    }
-    // Hasn't been loaded previously, so go ahead and do so now.
-    else
-    {
-        // Load the BITMAP from file and take ownership of it
-        pReturnBitmap = LoadAndReleaseBitmap(conversionMode);
-
-        // Now when loaded for the first time, enter into the map, PASSING OVER OWNERSHIP OF THE LOADED DATAFILE
-        m_sLoadedBitmaps.insert(pair<string, BITMAP *>(m_DataPath, pReturnBitmap));
-    }
-
-    // Return without transferring ownership
-    return pReturnBitmap;
-}
-
-
-
-//////////////////////////////////////////////////////////////////////////////////////////
-// Virtual method:  LoadAndReleaseBitmap
-//////////////////////////////////////////////////////////////////////////////////////////
-// Description:     Loads and transfers the data represtented by this ConentFile object as
-//                  an Allegro BITMAP. Note that ownership of the BITMAP IS TRANSFERRED!
-//                  Also, this is relatively slow since it reads the data from disk each time.
-
-BITMAP * ContentFile::LoadAndReleaseBitmap(int conversionMode)
-{
-    if (m_DataPath.empty())
-        return 0;
-
-    BITMAP *pReturnBitmap = 0;
-    // Set the conversion mode, default to COLORCONV_MOST if none was set
-    set_color_conversion(conversionMode == 0 ? COLORCONV_MOST : conversionMode);
-
-    // Find where the '#' denoting the divider between the datafile and the datafile object's name is
-    int separatorPos = m_DataPath.rfind('#');
-
-    // If there is none, that means we're told to load an exposed file outside of a .dat datafile.
-    if (separatorPos == -1)
-    {
-        PACKFILE *pFile = pack_fopen(m_DataPath.c_str(), F_READ);
-        // Make sure we opened properly, or try to add 000 before the extension if it's part of an animation naming
-        if (!pFile)
-        {
-            // Truncate away the extension
-            int extensionPos = m_DataPath.rfind('.');
-            m_DataPath.resize(extensionPos);
-            pFile = pack_fopen((m_DataPath + "000.bmp").c_str(), F_READ);
-            
-        }
-
-        // Get the current color palette; the load_bmp function needs it
-        PALETTE currentPalette;
-        get_palette(currentPalette);
-
-        // Now load the actual BITMAP
-        pReturnBitmap = load_bmp_pf(pFile, (RGB *)currentPalette);
-
-        // Close the file stream
-        pack_fclose(pFile);
-    }
-    return pReturnBitmap;
-}
+		PALETTE currentPalette;
+		get_palette(currentPalette);
+		return load_bmp(m_DataPath.c_str(), currentPalette);
+	}
 }
--- Wrappers/ContentFile.h
+++ Wrappers/ContentFile.h
@@ -7,45 +7,28 @@ struct BITMAP;
 
 namespace RTE {
 
-	/// <summary>
-	/// A representation of a content file that is stored either directly on on disk or packaged in another file.
-	/// </summary>
+/// <summary>
+/// A representation of a content file that is stored either directly on on disk or packaged in another file.
+/// </summary>
 class ContentFile : public Serializable {
 
 public:
 
-
-//////////////////////////////////////////////////////////////////////////////////////////
-// Constructor:     ContentFile
-//////////////////////////////////////////////////////////////////////////////////////////
-// Description:     Constructor method used to instantiate a ContentFile object in system
-//                  memory. Create() should be called before using the object.
-// Arguments:       None.
-
+	/// <summary>
+	/// Constructor method used to instantiate a ContentFile object in system memory. Create() should be called before using the object.
+	/// </summary>
     ContentFile() { Clear(); }
 
-
-//////////////////////////////////////////////////////////////////////////////////////////
-// Constructor:     ContentFile
-//////////////////////////////////////////////////////////////////////////////////////////
-// Description:     Constructor method used to instantiate a ContentFile object in system
-//                  memory, and also do a Create() in the same line. Create() should
-//                  therefore not be called after using this constructor.
-// Arguments:       A string defining the path to where the content file itself is located,
-//                  either within the package file, or directly on the disk.
-//                  A directory
-
+	/// <summary>
+	/// Constructor method used to instantiate a ContentFile object in system memory, and also do a Create() in the same line. Create() should therefore not be called after using this constructor.
+	/// </summary>
+	/// <param name="filePath">A string defining the path to where the content file itself is located, either within the package file, or directly on the disk.</param>
     ContentFile(const char *filePath) { Clear(); Create(filePath); }
 
-
-//////////////////////////////////////////////////////////////////////////////////////////
-// Destructor:      ~ContentFile
-//////////////////////////////////////////////////////////////////////////////////////////
-// Description:     Destructor method used to clean up a ContentFile object before deletion
-//                  from system memory.
-// Arguments:       None.
-
-    virtual ~ContentFile() { Destroy(true); }
+	/// <summary>
+	/// Destructor method used to clean up a ContentFile object before deletion from system memory.
+	/// </summary>
+    virtual ~ContentFile() { Destroy(); }
 
 //////////////////////////////////////////////////////////////////////////////////////////
 // Virtual method:  Create
@@ -92,7 +75,7 @@ public:
 //                  to destroy all inherited members also.
 // Return value:    None.
 
-    virtual void Destroy(bool notInherited = false);
+    void Destroy() { Clear(); }
 
 
 //////////////////////////////////////////////////////////////////////////////////////////
@@ -113,7 +96,7 @@ public:
 // Arguments:       None.
 // Return value:    A string with the friendly-formatted type name of this object.
 
-    virtual const std::string & GetClassName() const { return m_ClassName; }
+    const std::string & GetClassName() const { return m_ClassName; }
 
 
 //////////////////////////////////////////////////////////////////////////////////////////
@@ -124,7 +107,7 @@ public:
 // Arguments:       A string with the datafile object name path
 // Return value:    None.
 
-    virtual void SetDataPath(std::string newDataPath);
+    void SetDataPath(std::string newDataPath) { m_DataPath = newDataPath; }
 
 
 //////////////////////////////////////////////////////////////////////////////////////////
@@ -135,17 +118,8 @@ public:
 // Arguments:       None.
 // Return value:    A string with the datafile object name path
 
-    virtual const std::string & GetDataPath() const { return m_DataPath; };
+    const std::string & GetDataPath() const { return m_DataPath; };
 
-
-//////////////////////////////////////////////////////////////////////////////////////////
-// Method:          GetDataModuleID
-//////////////////////////////////////////////////////////////////////////////////////////
-// Description:     Gets the ID of the Data Module this file is inside.
-// Arguments:       None.
-// Return value:    The ID of the Data Module containing this' file.
-
-    int GetDataModuleID();
 
 //////////////////////////////////////////////////////////////////////////////////////////
 // Virtual method:  GetAsBitmap
@@ -161,7 +135,7 @@ public:
 //                  .dat datafile. file.
 //                  Ownership is NOT transferred! If 0, the file could not be found/loaded.
 
-    virtual BITMAP * GetAsBitmap(int conversionMode = 0);
+    BITMAP * GetAsBitmap(int conversionMode = 0);
 
 
 
@@ -177,34 +151,21 @@ public:
 // Return value:    The pointer to the BITMAP loaded from disk Ownership IS transferred!
 //                  If 0, the file could not be found/loaded.
 
-    virtual BITMAP * LoadAndReleaseBitmap(int conversionMode = 0);
+    BITMAP * LoadAndReleaseBitmap(int conversionMode = 0);
 
 
 protected:
 
     static const std::string m_ClassName;
-
-    // Static map containing all the already loaded BITMAP:s and their paths
-    static std::map<std::string, BITMAP *> m_sLoadedBitmaps;
-
-    // Path to this ContentFile's Datafile Object's path.
-    // In the case of an animation, this filename/name will be appended with 000, 001, 002 etc
-    std::string m_DataPath;
-
-    // Data Module ID of where this was loaded from
-    int m_DataModuleID;
+    static std::unordered_map<std::string, BITMAP *> m_LoadedBitmaps; // Static map containing all the already loaded BITMAP:s and their paths
+    std::string m_DataPath; // Path to this ContentFile's Datafile Object's path.
 
 private:
 
-//////////////////////////////////////////////////////////////////////////////////////////
-// Method:          Clear
-//////////////////////////////////////////////////////////////////////////////////////////
-// Description:     Clears all the member variables of this ContentFile, effectively
-//                  resetting the members of this abstraction level only.
-// Arguments:       None.
-// Return value:    None.
-
-    void Clear();
+	/// <summary>
+	/// Clears all the member variables of this ContentFile, effectively resetting the members of this abstraction level only.
+	/// </summary>
+    void Clear() { m_DataPath.erase(); }
 };
 }
 #endif
