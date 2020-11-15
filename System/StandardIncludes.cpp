3d6f7e05554a4993b7c2810cb96bb606df2d9c9d
MaximDude <maximdude@gmail.com>
2020-11-15 04:12:05 +0200

    Ditch compiling GUI as static library

--- System/StandardIncludes.cpp
+++ System/StandardIncludes.cpp
@@ -1,5 +1,5 @@
 // LibraryIncludes.cpp: source file corresponding to the pre-compiled header
 
-#include "LibraryIncludes.h"
+#include "StandardIncludes.h"
 
 // When you are using pre-compiled headers, this source file is necessary for compilation to succeed.
