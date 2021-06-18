3eec74d7549f3c6d7726de1b9ea08c1cbae96fb8
MaximDude <maximdude@gmail.com>
2021-06-18 13:36:34 +0300

    Refactor AllegroInput

--- GUI/Wrappers/AllegroInput.h
+++ GUI/Wrappers/AllegroInput.h
@@ -1,125 +1,73 @@
 #ifndef _ALLEGROINPUT_
 #define _ALLEGROINPUT_
 
-//////////////////////////////////////////////////////////////////////////////////////////
-// File:            AllegroInput.h
-//////////////////////////////////////////////////////////////////////////////////////////
-// Description:     AllegroInput Class
-// Project:         GUI Library
-// Author(s):       Jason Boettcher
-//                  jackal@shplorb.com
-//                  www.shplorb.com/~jackal
-
 #include "GUIInput.h"
 
+namespace RTE {
 
-enum aMButtons {
-    AMBLEFT = 1,
-    AMBRIGHT = 2,
-    AMBMIDDLE = 4
+	class Timer;
+
+	/// <summary>
+	/// An input interface using the CDX Library for input GUIInput.
+	/// </summary>
+	class AllegroInput : public GUIInput {
+
+	public:
+
+		/// <summary>
+		/// Constructor method used to instantiate a AllegroInput object in system memory.
+		/// </summary>
+		/// <param name="whichPlayer"></param>
+		/// <param name="keyJoyMouseCursor">Whether the keyboard and joysticks also can control the mouse cursor.</param>
+		AllegroInput(int whichPlayer, bool keyJoyMouseCursor = false);
+
+		/// <summary>
+		/// Changes the mouse movement speed based on the graphics driver in use. The default (2) movement speed is slow and sluggish in dedicated fullscreen so this is used to change between default and faster (1) when switching drivers.
+		/// </summary>
+		/// <param name="graphicsDriver">The graphics driver to adjust mouse movement speed to.</param>
+		void AdjustMouseMovementSpeedToGraphicsDriver(int graphicsDriver) const;
+
+		/// <summary>
+		/// Updates the input.
+		/// </summary>
+		void Update() override;
+
+	private:
+
+		enum AllegroMouseButtons {
+			ButtonLeft = 1,
+			ButtonRight = 2,
+			ButtonMiddle = 4
+		};
+
+		const float m_KeyRepeatDelay = 0.300F;
+		std::array<float, 256> m_KeyboardTimes;
+
+		std::unique_ptr<Timer> m_Timer;
+		std::unique_ptr<Timer> m_CursorAccelTimer;
+
+		/// <summary>
+		/// Converts from Allegro's key push to that used by this GUI lib, with timings for repeats taken into consideration.
+		/// </summary>
+		/// <param name="allegroKey">The Allegro scancode.</param>
+		/// <param name="guilibKey">The corresponding GUIlib scancode</param>
+		/// <param name="elapsedS">The elapsed time since the last update, in seconds.</param>
+		void ConvertKeyEvent(int allegroKey, int guilibKey, float elapsedS);
+
+		/// <summary>
+		/// Updates the keyboard input.
+		/// </summary>
+		void UpdateKeyboardInput(float keyElapsedTime);
+
+		/// <summary>
+		/// Updates the mouse input.
+		/// </summary>
+		void UpdateMouseInput();
+
+		/// <summary>
+		/// Updates the mouse input using the joystick or keyboard.
+		/// </summary>
+		void UpdateKeyJoyMouseInput(float keyElapsedTime);
+	};
 };
-
-namespace RTE
-{
-
-class Timer;
-
-
-//////////////////////////////////////////////////////////////////////////////////////////
-// Class:           AllegroInput
-//////////////////////////////////////////////////////////////////////////////////////////
-// Description:     An input interface using the CDX Library for input
-// Parent(s):       GUIInput.
-// Class history:   12/30/2003 AllegroInput Created.
-//                  11/09/2005 Ported to use Allegro and renamed to AllegroInput.
-
-class AllegroInput :
-    public GUIInput
-{
-
-//////////////////////////////////////////////////////////////////////////////////////////
-// Public member variable, method and friend function declarations
-
-public:
-
-//////////////////////////////////////////////////////////////////////////////////////////
-// Constructor:     AllegroInput
-//////////////////////////////////////////////////////////////////////////////////////////
-// Description:     Constructor method used to instantiate a AllegroInput object in system
-//                  memory.
-// Arguments:       Whether the keyboard and joysticks also can control the mouse crsr.
-
-    AllegroInput(int whichPlayer, bool keyJoyMouseCursor = false);
-
-
-//////////////////////////////////////////////////////////////////////////////////////////
-// Destructor:      ~AllegroInput
-//////////////////////////////////////////////////////////////////////////////////////////
-// Description:     Destructor method used to clean up a AllegroInput object.
-// Arguments:       None.
-
-    ~AllegroInput();
-
-
-//////////////////////////////////////////////////////////////////////////////////////////
-// Method:         Update
-//////////////////////////////////////////////////////////////////////////////////////////
-// Description:     Updates the Input.
-// Arguments:       None.
-
-    void Update(void);
-
-
-//////////////////////////////////////////////////////////////////////////////////////////
-// Private member variable and method declarations
-
-private:
-
-
-//////////////////////////////////////////////////////////////////////////////////////////
-// Method:          ConvertKeyEvent
-//////////////////////////////////////////////////////////////////////////////////////////
-// Description:     Converts from allegro's key push to that used by this GUI lib, with
-//                  timings for repats taken into consideration.
-// Arguments:       The allegro scancode, the corrsponding guilib scancode, and the elapsed
-//                  time in S since last update.
-
-//    int ConvertKeyEvent(bool keyDown, int oldKeyEvent) { return keyDown ? (oldKeyEvent == Pushed ? Repeat : Pushed) : ((oldKeyEvent == Pushed || oldKeyEvent == Repeat) ? Released : None); }
-    void ConvertKeyEvent(int allegroKey, int guilibKey, float elapsedS);
-
-
-//////////////////////////////////////////////////////////////////////////////////////////
-// Method:         CreateTimer
-//////////////////////////////////////////////////////////////////////////////////////////
-// Description:     Creates the timer.
-// Arguments:       None.
-
-    bool CreateTimer(void);
-
-
-//////////////////////////////////////////////////////////////////////////////////////////
-// Method:         GetCurrentTime
-//////////////////////////////////////////////////////////////////////////////////////////
-// Description:     Gets the current time since the timer was created.
-// Arguments:       None.
-
-    float GetTime(void);
-
-    // Members
-    float       m_KeyDelay;
-    float       m_KeyRepeat;
-
-    int         m_ModStates[6];
-    float       m_KeyboardTimes[256];
-
-    // Timers
-    Timer       *m_pTimer;
-    Timer       *m_pCursorAccelTimer;
-};
-
-
-}; // namespace RTE
-
-
-#endif // File
- 
+#endif
