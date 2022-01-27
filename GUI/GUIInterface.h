8605d0da5408a74dc202af3963a6c2fb7bc73509
MaximDude <maximdude@gmail.com>
2022-01-27 02:09:42 +0200

    Move GUIRect into GUIInterface

--- GUI/GUIInterface.h
+++ GUI/GUIInterface.h
@@ -7,6 +7,23 @@ struct BITMAP;
 
 namespace RTE {
 
+#pragma region Rectangle Structure
+	/// <summary>
+	/// The GUIRect structure defines a rectangle by the coordinates of its upper-left and lower-right corners.
+	/// </summary>
+	struct GUIRect { int left; int top; int right; int bottom; };
+
+	/// <summary>
+	/// Sets the bounds of a GUIRect.
+	/// </summary>
+	/// <param name="pRect">Pointer to the GUIRect.</param>
+	/// <param name="left">Position of top left corner on X axis.</param>
+	/// <param name="top">Position of top left corner on Y axis.</param>
+	/// <param name="right">Position of bottom right corner on X axis.</param>
+	/// <param name="bottom">Position of bottom right corner on Y axis.</param>
+	inline void SetRect(GUIRect *rect, int left, int top, int right, int bottom) { rect->left = left; rect->top = top; rect->right = right; rect->bottom = bottom; }
+#pragma endregion
+
 #pragma region GUIBitmap
 	/// <summary>
 	/// An interface class inherited by the different types of bitmap methods.
