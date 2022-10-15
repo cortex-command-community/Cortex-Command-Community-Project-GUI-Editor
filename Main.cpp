21fb7dc884757e35a51197318b32db7383340508
MaximDude <maximdude@gmail.com>
2022-10-15 04:07:31 +0300

    Yeet all the things

--- Main.cpp
+++ Main.cpp
@@ -12,22 +12,22 @@ using namespace RTEGUI;
 /// Entry point for the GUI editor app.
 /// </summary>
 int APIENTRY WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow) {
-	allegro_init();
-	loadpng_init();
-	g_GUIEditor.Initialize();
+	//allegro_init();
+	//loadpng_init();
+	//g_GUIEditor.Initialize();
 
-	std::chrono::steady_clock::time_point frameTimeStart;
-	while (true) {
-		if (!g_GUIEditor.UpdateEditor()) {
-			break;
-		}
-		frameTimeStart = std::chrono::high_resolution_clock::now();
-		vsync();
-		g_GUIEditor.DrawEditor();
-		EditorApp::s_FrameTime = std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::high_resolution_clock::now() - frameTimeStart).count();
-	}
-	g_GUIEditor.DestroyBackBuffers();
-	ContentFile::FreeAllLoaded();
+	//std::chrono::steady_clock::time_point frameTimeStart;
+	//while (true) {
+	//	if (!g_GUIEditor.UpdateEditor()) {
+	//		break;
+	//	}
+	//	frameTimeStart = std::chrono::high_resolution_clock::now();
+	//	vsync();
+	//	g_GUIEditor.DrawEditor();
+	//	EditorApp::s_FrameTime = std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::high_resolution_clock::now() - frameTimeStart).count();
+	//}
+	//g_GUIEditor.DestroyBackBuffers();
+	//ContentFile::FreeAllLoaded();
 
 	std::exit(EXIT_SUCCESS);
 }
